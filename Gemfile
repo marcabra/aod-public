bd0d6beaddbf6558b661f7cbdef20b18f35729ea
Carlos Hernandez <carlos@recrea.es>
2013-11-30 13:11:22 +0000

    First commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'jquery-rails'
+gem 'pg'
+gem 'rails', '3.2.12'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+  # gem 'therubyracer', :platforms => :ruby
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'debugger'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aod::Application.config.secret_token = 'ab8e146121388b1e514339b5c23796baef84c55c92cad0dd8f93a36c0d5b943db2b8fd140e3f632d70f9b6054a3f0449bc1b086ef4f4c835eeb57a67c9c9f67d'

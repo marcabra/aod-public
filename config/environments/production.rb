e99260129a9e7f6837e0f3eb32d970688681b05b
Carlos Hernandez <carlos@recrea.es>
2013-12-07 14:06:22 +0000

    Edit production environment config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,11 +11,8 @@ Aod::Application.configure do
   # Eager load
   config.eager_load = true
   
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
-
   # Compress JavaScripts and CSS
-  config.assets.js_compressor = :uglifier
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
@@ -23,8 +20,14 @@ Aod::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  # Defaults to nil and saved in location specified by config.assets.prefix
+  # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
+
+  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  # config.assets.precompile += %w( search.js )
+
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # config.force_ssl = true
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache

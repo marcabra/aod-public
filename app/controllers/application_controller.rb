f822763c7c76769f6a4475aa3b8133f7b4ddbb86
Carlos Hernandez <carlos@recrea.es>
2013-12-11 19:52:14 +0000

    Protect code with http authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with :name => "aod", :password => "inprogress"
   protect_from_forgery
   before_filter :set_default_year  
   helper_method :current_year

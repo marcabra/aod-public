5d986b718c5cc4449e3dab9417fea7470c070b68
Carlos Hernandez <carlos@recrea.es>
2014-01-13 00:45:05 +0000

    Fix: projects list is not ordered

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -9,7 +9,7 @@ class TopicsController < ApplicationController
     
     @regions = Region.by_topic(@topic, current_year)
     @agencies = Agency.by_topic(@topic, current_year)    
-    @projects = @topic.projects.by_year(current_year).order_by_amount.limit(5)
+    @projects = @topic.projects.by_year(current_year).order("paid_amount desc").limit(5)
 
     gon.aids = @topic.aids.group("year").select("cast(year as varchar), sum(aids.paid_amount) as amount")
   end
--- app/models/project.rb
+++ app/models/project.rb
@@ -8,7 +8,6 @@ class Project < ActiveRecord::Base
   scope :group_by_regions, joins([:region, :aids]).group("regions.id").select("regions.id, regions.name, sum(aids.paid_amount) as amount").order("amount desc")
   scope :group_by_topics, joins([:topic, :aids]).group("topics.id").select("topics.id, topics.name, sum(aids.paid_amount) as amount").order("amount desc")
   scope :group_by_agencies, joins([:agencies, :aids]).group("agencies.id").select("agencies.id, agencies.name, sum(aids.paid_amount) as amount").order("amount desc")
-  scope :order_by_amount, joins(:aids).order("aids.paid_amount desc")
   
   def committed_amount
     @committed_amount ||= aids.sum(:committed_amount)
